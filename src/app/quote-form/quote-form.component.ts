fc6043116a32966226a0a4edaf639c288d73ef31
Perry <perryobara@gmail.com>
2019-04-30 11:02:44 +0300

    adds the event emitter to the quotes component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,7 +8,10 @@ import {Quote} from '../quote'
 })
 export class QuoteFormComponent implements OnInit {
   newQuote=new Quote(0,"","",new Date(),0);
-
+  @Output() addQuote=new EventEmitter<Quote>();
+    submitQuote(){
+      this.addQuote.emit(this.newQuote);
+    }
 
   constructor() { }
 

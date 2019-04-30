1da64d8f724d20cfbabec7110d320c3853f0312e
Perry <perryobara@gmail.com>
2019-04-30 12:34:14 +0300

    adds additional styling to the delet button

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription:boolean;
-  constructor(public id:number,public name:string,public description:string,public entry_date:Date,public vote:number){
+  constructor(public name:string,public description:string,public entry_date:Date,public vote:number,public submission:string){
     this.showDescription=false
   }
 }
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes =[
+    new Quote('Watching Finding Nemo','Finding an online version',new Date(2018,3,16),0,""),
+    new Quote('Watching Finding Nemo','Finding an online version',new Date(2018,4,18),0,""),
+    new Quote('Watching Finding Nemo','Finding an online version',new Date(2018,5,17),0,""),
+    new Quote('Watching Finding Nemo','Finding an online version',new Date(2018,6,14),0,""),
+  ]
+  toogleDetails(index){
+    this.quotes[index].showDescription= !this.quotes[index].showDescription;
+  }
+    deleteQuote(isComplete,index){
+      if(isComplete){
+        let toDelete=confirm('Are you sure you want to delete {{this.quotes[index].name}}')
+        if (toDelete){
+        this.quotes.splice(index,1);
+      }
+    }
+}
+      addNewQuote(quote){
+        quote.entry_date=new Date(quote.entry_date)
+        this.quotes.push(quote)
+      }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
